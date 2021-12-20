8b28d3d3bd0a6b732a5bd1f20721cbb7b833ebf9
Dylan Duan <90183195+dylduan-UNSW@users.noreply.github.com>
2021-12-20 17:51:44 +1100

    Get raw weather API data from sample link

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,9 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class APITests {
 
     @Test
     void test1(){
+        RestAssured.useRelaxedHTTPSValidation();
+        Response response = RestAssured.get("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en");
+        response.getStatusCode();
+        response.getBody();
+        System.out.println("Response :" + response.asString());
+        System.out.println("status code: "+response.getStatusCode());
+        System.out.println("body: " + response.getBody().asString());
+        System.out.println("Time taken: " + response.getTime());
+        System.out.println("header: " + response.getHeader("content-type"));
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
 
     }
 
