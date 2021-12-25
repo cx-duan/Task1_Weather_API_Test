2f876221004bc53c3e0f43cce9f99092d2de5539
Dylan Duan <90183195+dylduan-UNSW@users.noreply.github.com>
2021-12-25 22:06:43 +1100

    Final test case design
    Pending more adjustments - at this point I have most of the tests I think are good to run

--- src/test/java/WeatherAPITests.java
+++ src/test/java/WeatherAPITests.java
@@ -1,131 +1,132 @@
 import static io.restassured.RestAssured.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.fasterxml.jackson.databind.*;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-import io.restassured.http.Header;
-import io.restassured.http.Headers;
-import io.restassured.http.Method;
-//import io.restassured.mapper.ObjectMapper;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
-import jdk.nashorn.internal.parser.JSONParser;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-import java.io.FileReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class WeatherAPITests {
 
-    final static String url="https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en";
+    final static String url="https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en";
+    final static String baseURI = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php";
+    private static SoftAssert softAssert = new SoftAssert();
 
+    //Main test class. Variables are reusable and easily scalable within the API.
+    //Aim is to provide a generic API test, validating response, status, headers.
     @Test
     public void DailyWeatherReportTests(){
-    APISetup();
-        getBodyDetails();
-        getStatusLine(200);
-        getHeaders("application/json; charset=utf-8", "Apache", null);
-        //allHeaderDetails();
+        getResponseTime();
+        getResponseBody("rhrread","en");
+        getValidateResponseStatus("rhrread","en");
+        getValidateResponseHeaders("application/json; charset=utf-8","Apache");
+        validateResponseNotNull("rainfall");
+        validateResponseNotNull("warningMessage.data");
+        validateExtractedValue("rainfall.data", "place", "Kwai Tsing");
+        validateExtractedValue("temperature.data","unit","C");
 
-            }
-
-
-
-    //Reusable Method for API Setup with Relaxed HTTPS Validation
-    public static void APISetup(){
-        //Address the SSL Handshake error due to unrecognised certificate
-        useRelaxedHTTPSValidation();
-        // Specify the base URL to the RESTful web service
-        baseURI = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php";
     }
 
-    public static void getBodyDetails()
-    {
-        APISetup();
-        // Get the RequestSpecification of the request that you want to sent
-        // to the server. The server is specified by the BaseURI that we have
-        // specified in the above step.
-        RequestSpecification httpRequest = given();
-        // Make a request to the server by specifying the method Type and the method URL.
-        // This will return the Response from the server. Store the response in a variable.
-        Response response = httpRequest.request(Method.GET, "?dataType=rhrread&lang=en");
-        // Now let us print the body of the message to see what response
-        // we have received from the server
-        String responseBody = response.getBody().asString();
-        System.out.println("Response Body is =>  " + responseBody);
+    //Get response time
+    public static void getResponseTime(){
+        System.out.println("The time taken to fetch the response: "+get(url)
+                .timeIn(TimeUnit.MILLISECONDS) + " milliseconds");
     }
-    public static void getStatusLine(int expectedValue)
-    {
-        APISetup();
-        RequestSpecification httpRequest = given();
-        Response response = httpRequest.request(Method.GET, "?dataType=rhrread&lang=en");
 
-        // Get the status code from the Response. A successful interaction with the webservice will return status code: 200
-        int statusCode = response.getStatusCode();
-        // Assert that correct status code is returned.
-        Assert.assertEquals(statusCode /*actual value*/, expectedValue /*expected value*/, "Correct status code returned");
-        System.out.println("Status Code: " + statusCode);
+            //Get JSON Response body - This returns the entire body of the API response.
+            // There isn't a validation here but will include some for the nested queries to ensure that the values
+            // don't have any issues.
+    public static void getResponseBody(String dataType, String lang){
+        System.out.println(dataType +" Response Body is => ");
+        //Specify query parameters for the API. In this case, dataType and lang are the variables: rhrread, en.
+        // Changing these variables return different response bodies from the API
+        //Fetch all the response fetched including log response, headers etc.
+        ValidatableResponse responseBody = given().queryParam("dataType",dataType)
+                .queryParam("lang",lang)
+                .when().get(baseURI).then().log()
+                .body();
+        //Validates that the Response Body is not null
+        Assert.assertNotNull(responseBody, " Response body is null");
+
     }
-    public static void getHeaders(String expectedContentType,String expectedServerType, String expectedContentEncoding)
-    {
-        APISetup();
-        RequestSpecification httpRequest = given();
-        Response response = httpRequest.request(Method.GET, "?dataType=rhrread&lang=en");
 
-        // Reader header of a give name. In this line we will get Header named Content-Type
-        String contentType = response.header("Content-Type");
+        //Get the response status. The most basic validation is to check if the status code of the request is in the
+        // 2XX format
+    public static void getValidateResponseStatus(String dataType, String lang){
+        // Get the status code from the Response. A successful interaction with the webservice will
+        // return status code: 200
+        int statusCode= given().queryParam("dataType",dataType)
+                .queryParam("lang",lang) .when().get(baseURI).getStatusCode();
+        System.out.println("The response status is "+statusCode);
+        // Assert that correct status code is returned. If returns another status code it will have an error.
+        given().when().get(url).then().assertThat().statusCode(200);
+    }
+
+        // Get and validate the response headers. For this test case, I've chosen the Content Type and Server Type.
+        // If there are discrepancies, it can indicate that the HTTP is has threatening content.
+        // Ensures that only properly configured name-value pairs appear in the headers
+        //There is a lot of flexibility in the code to add more headers if needed.
+    public static void getValidateResponseHeaders(String expectedContentType, String expectedServerType){
+        //print all headers in the response
+        System.out.println("The headers in the response "+
+                get(url).then().extract().headers());
+
+        //Extract the content type and validate it to the expected value. Deviation should raise an error that needs
+        // further attention. Future header validations can follow the same format.
+        String contentType = get(url).then().extract().contentType();
         Assert.assertEquals(contentType /* actual value */, expectedContentType /* expected value */);
-        System.out.println("Content-Type value: " + contentType);
 
-        // Reader header of a give name. In this line we will get Header named Server
-        String serverType =  response.header("Server");
+        String serverType = get(url).then().extract().header("Server");
         Assert.assertEquals(serverType /* actual value */, expectedServerType /* expected value */);
-        System.out.println("Server value: " + serverType);
 
-        // Reader header of a give name. In this line we will get Header named Content-Encoding
-        String contentEncoding = response.header("Content-Encoding");
-        Assert.assertEquals(contentEncoding /* actual value */, expectedContentEncoding /* expected value */);
-        System.out.println("Content-Encoding: " + contentEncoding);
     }
-    public void allHeaderDetails()
-    {
-        APISetup();
-        RequestSpecification httpRequest = given();
-        Response response = httpRequest.request(Method.GET, "?dataType=rhrread&lang=en");
 
-        // Get all the headers. Return value is of type Headers.
-        // Headers class implements Iterable interface, hence we
-        // can apply an advance for loop to go through all Headers
-        // as shown in the code below
-        Headers allHeaders = response.headers();
-
-        // Iterate over all the Headers
-        for(Header header : allHeaders)
-        {
-            System.out.println("Key: " + header.getName() + " Value: " + header.getValue());
+    //Check response is not null with a given parameter. Flexible adjustments can be
+    // made at the test method
+    public static void validateResponseNotNull(String parameter)throws IllegalArgumentException{
+        try {
+            get(url)
+                    .then()
+                    .body(parameter, notNullValue()
+                    );
+        }catch(Exception e){
+            softAssert.fail("Parameter not defined");
+            System.out.println(e);
         }
     }
 
+    //@Test
+    public void testGetStringValue() {
+        ArrayList<String> test =
+        get(url)
+                .then()
+                .extract()
+                .path("uvindex");
+        System.out.println(test);
+
+    }
+
+    //Specific validation for nested queries. Used when the tester wants to verify a value.
+    public void validateExtractedValue(String path, String data, String valueToFind) {
+        //Extract specific value from path using method variables that can be adjusted in the main
+        //test method. As the process finds the extracted value
+        String extractedValue =
+                get(url)
+                        .then()
+                        .extract()
+                        .response()
+                        .path( path+".find { it."+data+" == '"+valueToFind+"' }."+data);
+        if(extractedValue != null) {
+            System.out.println("Found extracted value from " + path + "-" + data + ": " + extractedValue);
+        }
+        Assert.assertNotNull(extractedValue,"Extracted value not found");
+    }
 
 }
